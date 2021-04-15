7ba70bebcc1333564da389879b57d00c4b0fdcc7
eric-delguerra <47738252+eric-delguerra@users.noreply.github.com>
2021-04-15 13:34:31 +0200

    add routes + Procfile

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-
+const express = require('express')
+const app = express()
 let firebase = require('firebase')
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-let firebaseConfig = {
-    apiKey: "AIzaSyDpjkAd12e3OdvsGpA1okozUv2YFNaDEmU",
-    authDomain: "testconstruct-d38e2.firebaseapp.com",
-    projectId: "testconstruct-d38e2",
-    storageBucket: "testconstruct-d38e2.appspot.com",
-    messagingSenderId: "346430428056",
-    appId: "1:346430428056:web:632e4e26c74d7e3a15d1ed"
-};
+
 
 let firebaseConfig = {
     apiKey: process.env.APIKEY,
@@ -37,8 +31,11 @@ let database = firebase.database()
 //     }
 // })
 
-database.ref('/').once('value')
-    .then(function(snapshot) {
-        console.log( snapshot.val() )
-    })
 
+
+app.get('/', (req, res) => {
+    res.send(database.ref('/').once('value')
+        .then(function(snapshot) {
+            return snapshot.val()
+        }))
+})
